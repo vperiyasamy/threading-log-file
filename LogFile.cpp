bd01e03fdd88dfc7c47421a605ef07a7a4733dc2
Viswesh Periyasamy <vperiyasamy@wisc.edu>
2015-06-10 23:15:44 -0500

    file i/o fix

--- LogFile.cpp
+++ LogFile.cpp
@@ -1,13 +1,13 @@
 // LogFile.cpp
 
-#include "log_file.h"
+#include "LogFile.h"
 
 bool LogFile::displayFlag = false; // to determine if we should print to cout
-std::fstream LogFile::log_file; // file object to open log.txt
+std::fstream LogFile::log_file("log.txt", std::ios_base::app); // file object to open log.txt
 std::stringstream LogFile::line; // string stream to capture info
 std::string LogFile::log_string; // to be written to file/cout
 std::chrono::high_resolution_clock::time_point LogFile::program_start; //used as reference for microsecond execution
-std::mutex mutex_lock; // to prevent multithreading file i/o error
+std::mutex LogFile::mutex_lock; // to prevent multithreading file i/o error
 
 // This method only called once to set up logging
 void LogFile::startLog(bool displayOn) {
@@ -27,6 +27,7 @@ void LogFile::startLog(bool displayOn) {
 	line << "Program started: " << date_stamp << "\n";
 	log_string = line.str();
 
+	log_file.open("log.txt", std::ios_base::trunc);
 	log_file.open("log.txt", std::ios_base::app);
 	log_file << log_string;
 	log_file.close();
